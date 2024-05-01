29189490867b061f86cf676d7096ba59e99a161a
Eranmonnie <ajalaferanmi123@gmail.com>
2024-05-01 21:05:50 +0100

    init 2

--- Models/connection.js
+++ Models/connection.js
@@ -5,6 +5,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PSWD,
   database: process.env.DB_NAME,
+  port: "3306"
 });
 
 connection.connect((err)=>console.log(err || 'Database Connection Established'))
